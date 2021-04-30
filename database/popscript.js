7d2134c98f352e4cd6e90d7f7d26ea57fbd6654a
Mical15 <Michael.Gallien2@gmail.com>
2021-04-29 20:32:09 -0700

    Completed Collection Generator, need to test

---
+++ database/popscript.js
@@ -0,0 +1,32 @@
+// const config = require('../config.js');
+const axios = require('axios');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+mongoose.connect('mongodb://localhost/FEC', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+});
+
+let campIds = []; 
+
+for (var i = 0; i < 100; i++) {
+    campIds.push(i);
+}
+
+const imageSchema = new mongoose.Schema({
+    userName: String,
+    created: String,
+    helpfulness: Number,
+    caption: String,
+    priority: String,
+    imageUrl: String,
+});
+
+const popDatabase = (names) => {
+    names.forEach((title) => {
+        const title = mongoose.model(title, imageSchema)
+    })
+}
