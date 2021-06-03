6278349fb3751bb964d9ee15e9608963eab8fbed
Michael Gallien <Michael.Gallien2@gmail.com>
2021-06-03 14:03:16 -0700

    Applying stash

--- client/src/components/images.jsx
+++ client/src/components/images.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 
   const Images = props => {
-
     return (
       <div style={{ position: 'relative', overflow: 'hidden' }}>
         <div className='images' style={{ maxWidth: '98%', maxHeight: '20%', overflow: 'hidden' }}>
-          <img className='1' src={props.data[0].imageUrl} onClick={props.showModal} width="400" height="250" style={{ float: 'left', paddingRight: '8px' }} />
-          <img src={props.data[1].imageUrl} onClick={props.showModal} width="400" height="250" style={{ float: 'left', paddingRight: '8px' }} />
-          <img src={props.data[2].imageUrl} onClick={props.showModal} width="400" height="250" style={{ float: 'left', paddingRight: '8px', position: 'absolute', display: 'inline' }} />
+          <img className='1' src={props.data[0]} onClick={props.showModal} width="400" height="250" style={{ float: 'left', paddingRight: '8px' }} />
+          <img src={props.data[1]} onClick={props.showModal} width="400" height="250" style={{ float: 'left', paddingRight: '8px' }} />
+          <img src={props.data[2]} onClick={props.showModal} width="400" height="250" style={{ float: 'left', paddingRight: '8px', position: 'absolute', display: 'inline' }} />
         </div>
       </div>
     );
